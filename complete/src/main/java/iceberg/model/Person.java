7ac19698776a1ae7d556422cd66cb8f2f59b61be
David Peterson <david@davidseth.net>
2013-11-12 16:53:39 +1000

    adding additional relationships - trying to getID mapping in JSON (not yet working)

--- complete/src/main/java/iceberg/model/Person.java
+++ complete/src/main/java/iceberg/model/Person.java
@@ -3,7 +3,12 @@ package iceberg.model;
 import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.Property;
 import com.tinkerpop.frames.VertexFrame;
-import hello.*;
+import com.tinkerpop.blueprints.Vertex;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tinkerpop.frames.annotations.gremlin.GremlinGroovy;
+import com.tinkerpop.frames.modules.javahandler.JavaHandler;
+import com.tinkerpop.frames.modules.javahandler.JavaHandlerContext;
 
 public interface Person extends VertexFrame {
     
@@ -13,7 +18,13 @@ public interface Person extends VertexFrame {
     @Property("name")
     public String setName(String name);
     
-    public long getId();
+//    @Property("id")
+//    @GremlinGroovy(value="it.asVertex().getId()", frame=false)
+//    public long getId();
+    
+    @Property("id")
+    @JavaHandler
+    public Object getId();    
     
     @Property("content")
     public String getContent();
@@ -21,11 +32,34 @@ public interface Person extends VertexFrame {
     @Property("content")
     public String setContent(String content);
     
-    @Adjacency(label = "family") Iterable<? extends Person> getFamily();
+    @Adjacency(label = "family")
+    Iterable<Person> getFamily();
+    //Iterable<? extends Person> getFamily();
     
-    @Adjacency(label = "family") void addFamily(Person person);
+    @Adjacency(label = "family")
+    void addFamily(Person person);
     
-    @Adjacency(label = "friends") Iterable<? extends Person> getFriends();
+    @Adjacency(label = "friends")
+    Iterable<Person> getFriends();
+    //Iterable<? extends Person> getFriends();
     
-    @Adjacency(label = "friends") void addPet(Person person);
+    @Adjacency(label = "friends")
+    void addFriend(Person person);
+    
+    @GremlinGroovy("it.as('x').out('created').in('created').except('x')")
+    public Iterable<Person> getCoCreators();  
+    
+    public abstract class Impl implements JavaHandlerContext<Vertex>, Person {
+//        public String getNameAndAge() {
+//          return getName() + " (" + getAge() + ")"; //Call other methods that are handled by other annotations.
+//        } 
+        
+        @Override
+        @JavaHandler
+        public Object getId() {
+            Vertex vertex = this.asVertex();
+          Object id = this.it().getId();
+          return id;
+        }        
+    }
 }
