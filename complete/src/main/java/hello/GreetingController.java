9f7adca095807d943028ade6f44878f515df27a8
David Peterson <david@davidseth.net>
2013-12-05 01:44:08 +1000

    Finally figured out why I wasn't getting ID, needed to put JavaHandlerModule into the createfactory method (facepalm)

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -14,12 +14,16 @@ import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanTransaction;
 import com.thinkaurelius.titan.core.TitanType;
 import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory;
+import com.tinkerpop.frames.FrameInitializer;
 import com.tinkerpop.frames.FramedGraph;
 import com.tinkerpop.frames.FramedGraphFactory;
 import com.tinkerpop.frames.modules.gremlingroovy.GremlinGroovyModule;
+import com.tinkerpop.frames.modules.javahandler.JavaHandlerModule;
+import com.tinkerpop.frames.modules.typedgraph.TypedGraphModuleBuilder;
 
 import org.apache.commons.configuration.BaseConfiguration;
 import org.apache.commons.configuration.Configuration;
@@ -31,6 +35,7 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    //private FramedGraph<Graph> framedGraph;
 
     @RequestMapping("/greeting")
     public @ResponseBody Greeting greeting(
@@ -76,6 +81,13 @@ public class GreetingController {
         return nicola;//.asVertex().getId();
     } 
     
+    public static FrameInitializer myInitializer = new FrameInitializer() {
+        @Override
+        public void initElement(Class<?> kind, FramedGraph<?> framedGraph, Element element) {
+            element.setProperty("class", kind.getName()); //For example: save the class name of the frame on the element
+        }
+    };   
+    
     @RequestMapping("/graph")
     public @ResponseBody String graph() {
         
@@ -86,8 +98,6 @@ public class GreetingController {
         conf.setProperty("storage.index.search.backend", "elasticsearch");
         conf.setProperty("storage.index.search.hostname", "127.0.0.1");
         conf.setProperty("storage.index.search.client-only", "true");
-        
-        
      
         TitanGraph g = TitanFactory.open(conf);
         
@@ -104,22 +114,49 @@ public class GreetingController {
         if (cityType == null) {
             g.makeKey("city").dataType(String.class).indexed(Vertex.class).indexed(Edge.class).indexed("search",Vertex.class).indexed("search",Edge.class).make();
         }
+
         
-        FramedGraphFactory factory = new FramedGraphFactory();
+        
+//        FramedGraph<TitanGraph> framedGraph = new FramedGraph(g);
+//        framedGraph.registerFrameInitializer(myInitializer);
+        
+        FramedGraphFactory factory = new FramedGraphFactory(new JavaHandlerModule());
         FramedGraph<TitanGraph> framedGraph = factory.create(g);
 
-        Person nicola = framedGraph.addVertex(null, Person.class);
+//        FramedGraphFactory factory = new FramedGraphFactory(
+//            new GremlinGroovyModule(),
+//            new TypedGraphModuleBuilder().withClass(Person.class).build());
+//        FramedGraph framedGraph = factory.create(g);  //Groovy and typed graph created.         
+//        
+        
+//        framedGraph = new FramedGraphFactory(new AbstractModule(){
+//                @Override
+//                protected void doConfigure(FramedGraphConfiguration config) {
+//                 config.addFrameInitializer(myInitializer);
+//                }
+//        }).create(g);        
+        
+       
+
+        Person nicola = (Person) framedGraph.addVertex(null, Person.class);
         nicola.setName("Nicola Peterson");        
        
-        Person david = framedGraph.addVertex(null, Person.class);
+        Person david = (Person) framedGraph.addVertex(null, Person.class);
         david.setName("David Peterson");
         david.addFamily(nicola);
         david.addFriend(nicola);
-        
-
-        
+       
         
         System.out.println( "david id: " + david.getId());
+        System.out.println( "nicola content: " + nicola.getName());
+
+//        if (g.isOpen() == false) {
+//            g = TitanFactory.open(conf);
+//            return "";
+//        }
+        
+        
+        
         
         Vertex juno = g.addVertex(null);
         juno.setProperty("name", "juno");
@@ -130,6 +167,11 @@ public class GreetingController {
         jupiter.setProperty("name", "jupiter");
         Edge friends = g.addEdge(null, juno, jupiter, "friends");
         Edge family = g.addEdge(null, juno, jupiter, "family");
+        
+        Person jupiterFrame = framedGraph.getVertex(jupiter.getId(), Person.class);
+        System.out.println( "jupiterFrame id: " + jupiterFrame.asVertex().getId());
+        System.out.println( "jupiterFrame content: " + jupiterFrame.getName());        
+        
         
         
         //return juno.toString();
